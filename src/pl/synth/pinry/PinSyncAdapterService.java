72fb7834ed962a71eb4d134746a20e171644956e
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2012-07-16 08:04:30 +0200

    Split image pulling code into a separate method in SyncAdapter
    Move all the code responsible for pulling images from the pinry instance
    into a separate method. Also, while doing that remove a call to the
    separate static method that handles sync and do all that inside the
    SyncAdapter implementation.

--- src/pl/synth/pinry/PinSyncAdapterService.java
+++ src/pl/synth/pinry/PinSyncAdapterService.java
@@ -2,7 +2,6 @@ package pl.synth.pinry;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
-import android.accounts.OperationCanceledException;
 import android.app.Service;
 import android.content.*;
 import android.database.Cursor;
@@ -13,7 +12,6 @@ import android.os.IBinder;
 import java.util.ArrayList;
 
 public class PinSyncAdapterService extends Service {
-    private static final String TAG = "PinSyncAdapterService";
     private static SyncAdapterImpl syncAdapter = null;
 
     @Override
@@ -29,13 +27,43 @@ public class PinSyncAdapterService extends Service {
             this.context = context;
         }
 
+        private void pullUpdates(Account account) {
+            ContentResolver contentResolver = context.getContentResolver();
+            AccountManager manager = AccountManager.get(context);
+            String url = manager.getUserData(account, "url");
+            NetworkClient client = new NetworkClient(url, context);
+            ArrayList<Pin> newPins = client.getPinsSince(0L);
+
+            ContentValues values = new ContentValues();
+
+            for (Pin pin : newPins) {
+                values.clear();
+
+                Uri uri = ContentUris.withAppendedId(Pinry.Pins.CONTENT_ID_URI_BASE, pin.getId());
+                Cursor c = contentResolver.query(uri, new String[]{Pinry.Pins._ID, Pinry.Pins.COLUMN_NAME_IMAGE_PATH}, null, null, null);
+                if (c.getCount() > 0) {
+                    c.moveToFirst();
+                    c.close();
+                    continue;
+                } else {
+                    c.close();
+                }
+
+                values.put(Pinry.Pins._ID, pin.getId());
+                values.put(Pinry.Pins.COLUMN_NAME_IMAGE_PATH, pin.getLocalPath());
+                values.put(Pinry.Pins.COLUMN_NAME_THUMBNAIL_PATH, pin.getThumbnailPath());
+                values.put(Pinry.Pins.COLUMN_NAME_DESCRIPTION, pin.getDescription());
+                values.put(Pinry.Pins.COLUMN_NAME_SYNC_STATE, Pinry.Pins.SyncState.SYNCED);
+                values.put(Pinry.Pins.COLUMN_NAME_SOURCE_URL, pin.getSourceUrl());
+                values.put(Pinry.Pins.COLUMN_NAME_PUBLISHED, pin.getPublishedDate());
+
+                contentResolver.insert(Pinry.Pins.CONTENT_URI, values);
+            }
+        }
+
         @Override
         public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
-            try {
-                PinSyncAdapterService.performSync(context, account, extras, authority, provider, syncResult);
-            } catch (OperationCanceledException e) {
-
-            }
+            pullUpdates(account);
         }
     }
 
@@ -44,39 +72,5 @@ public class PinSyncAdapterService extends Service {
             syncAdapter = new SyncAdapterImpl(this, true);
         }
         return syncAdapter;
-    }
-
-    private static void performSync(Context context, Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) throws OperationCanceledException {
-        ContentResolver contentResolver = context.getContentResolver();
-        AccountManager manager = AccountManager.get(context);
-        String url = manager.getUserData(account, "url");
-        NetworkClient client = new NetworkClient(url, context);
-        ArrayList<Pin> newPins = client.getPinsSince(0L);
-
-        ContentValues values = new ContentValues();
-
-        for(Pin pin : newPins) {
-            values.clear();
-
-            Uri uri = ContentUris.withAppendedId(Pinry.Pins.CONTENT_ID_URI_BASE, pin.getId());
-            Cursor c = contentResolver.query(uri, new String[] {Pinry.Pins._ID, Pinry.Pins.COLUMN_NAME_IMAGE_PATH}, null, null, null);
-            if (c.getCount() > 0) {
-                c.moveToFirst();
-                c.close();
-                continue;
-            } else {
-                c.close();
-            }
-
-            values.put(Pinry.Pins._ID, pin.getId());
-            values.put(Pinry.Pins.COLUMN_NAME_IMAGE_PATH, pin.getLocalPath());
-            values.put(Pinry.Pins.COLUMN_NAME_THUMBNAIL_PATH, pin.getThumbnailPath());
-            values.put(Pinry.Pins.COLUMN_NAME_DESCRIPTION, pin.getDescription());
-            values.put(Pinry.Pins.COLUMN_NAME_SYNC_STATE, Pinry.Pins.SyncState.SYNCED);
-            values.put(Pinry.Pins.COLUMN_NAME_SOURCE_URL, pin.getSourceUrl());
-            values.put(Pinry.Pins.COLUMN_NAME_PUBLISHED, pin.getPublishedDate());
-
-            uri = contentResolver.insert(Pinry.Pins.CONTENT_URI, values);
-        }
     }
 }
